96166b841867dcd2a217915166ccb16d518f334d
Zhu Chengbin <104408988+Mrzqd@users.noreply.github.com>
2025-09-04 14:22:29 +0800

    Comment out OpenClash setup commands
    Comment out commands for downloading and copying OpenClash.

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,9 +16,9 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-wget https://github.com/vernesong/OpenClash/archive/master.zip
+# wget https://github.com/vernesong/OpenClash/archive/master.zip
 ## 解压
-unzip master.zip
+# unzip master.zip
 ## 复制OpenClash软件包到OpenWrt
-cp -r OpenClash-master/luci-app-openclash package/
-ls package/
+# cp -r OpenClash-master/luci-app-openclash package/
+# ls package/
